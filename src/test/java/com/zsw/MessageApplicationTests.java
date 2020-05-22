c44083b9724a6454c1e7c5f04914570993ea1ead
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-05-22 11:56:21 +0800

    new

--- src/test/java/com/zsw/MessageApplicationTests.java
+++ src/test/java/com/zsw/MessageApplicationTests.java
@@ -1,13 +1,13 @@
 package com.zsw;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MessageApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
